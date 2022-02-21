b9b63100faae0c8a2aa36729139880f0f2cf1119
ajaypradeep <ajaypradeep1712@gmail.com>
2022-02-21 11:56:32 +0530

    data changes

--- src/components/Grid/index.js
+++ src/components/Grid/index.js
@@ -1,47 +1,51 @@
-import React, { useState } from 'react';
-import Paper from '@mui/material/Paper';
+import React, { useState } from "react";
+import Paper from "@mui/material/Paper";
 import {
   SelectionState,
   PagingState,
   IntegratedPaging,
   IntegratedSelection,
-} from '@devexpress/dx-react-grid';
+} from "@devexpress/dx-react-grid";
 import {
   Grid,
   Table,
   TableHeaderRow,
   TableSelection,
   PagingPanel,
-} from '@devexpress/dx-react-grid-material-ui';
+} from "@devexpress/dx-react-grid-material-ui";
 
-import { generateRows } from '../../demo-data/generator';
 
 export default () => {
   const [columns] = useState([
-    { name: 'name', title: 'Name' },
-    { name: 'gender', title: 'Gender' },
-    { name: 'city', title: 'City' },
-    { name: 'car', title: 'Car' },
+    { name: "name", title: "Name" },
+    { name: "gender", title: "Gender" },
+    { name: "city", title: "City" },
+    { name: "car", title: "Car" },
   ]);
-  const [rows] = useState(generateRows({ length: 8 }));
+  const [rows, setRows] = useState([
+    {
+      name: "ajay",
+      gender: "male",
+      city: "sby",
+      car: "polo",
+    },
+    {
+      name: "james",
+      gender: "male",
+      city: "any",
+      car: "audi",
+    }
+  ]);
+
+  console.log("");
   const [selection, setSelection] = useState([]);
 
   return (
     <div>
-      <span>
-        Total rows selected:
-        {' '}
-        {selection.length}
-      </span>
+      <span>Total rows selected: {selection.length}</span>
       <Paper>
-        <Grid
-          rows={rows}
-          columns={columns}
-        >
-          <PagingState
-            defaultCurrentPage={0}
-            pageSize={6}
-          />
+        <Grid rows={rows} columns={columns}>
+          <PagingState defaultCurrentPage={0} pageSize={6} />
           <SelectionState
             selection={selection}
             onSelectionChange={setSelection}
@@ -51,7 +55,7 @@ export default () => {
           <Table />
           <TableHeaderRow />
           <TableSelection showSelectAll />
-          
+
           <PagingPanel />
         </Grid>
       </Paper>
